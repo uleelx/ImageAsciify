a16b00104828e696a399b9f78d201caaec3ee10b
zengxiangbin <zengxiangbin@96900.com.cn>
2019-07-14 01:10:31 +0800

    init

---
+++ ImageAsciify/Asciify.cs
@@ -0,0 +1,66 @@
+﻿using PhotoSauce.MagicScaler;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp.Processing;
+using System;
+using System.IO;
+using System.Text;
+
+namespace ImageAsciify
+{
+	public class Asciify
+	{
+		public static string ImageToHTML(string filePath, bool histogramEqualization, int maxLength)
+		{
+			char[] chars = new char[] { 'W', 'M', 'Q', 'R', 'O', 'S', 'C', 'V', '?', ')', '>', '!', ':', ',', '.' };
+			StringBuilder sb = new StringBuilder();
+			string html_head = "<!DOCTYPE html><html><body style = \"font-family: Monospace;font-size: 2px;line-height: 50%;\">";
+			string html_tail = "</body></html>";
+			sb.Append(html_head);
+
+			var outStream = new MemoryStream();
+
+			MagicImageProcessor.ProcessImage(filePath, outStream, new ProcessImageSettings());
+
+			outStream.Seek(0, SeekOrigin.Begin);
+
+			using (Image<Rgba32> image = (Image<Rgba32>)Image.Load(outStream))
+			{
+				int m = Math.Max(image.Height, image.Width);
+				double delta = Math.Max(m * 1.0 / maxLength, 1);
+				int w = (int)(image.Width / delta);
+				int h = (int)(image.Height / delta);
+
+				if (histogramEqualization)
+				{
+					image.Mutate(x => x.HistogramEqualization());
+				}
+
+				image.Mutate(x => x
+					 .Resize(w, h)
+					 .Grayscale());
+
+				for (int y = 0; y < h; y++)
+				{
+					for (int x = 0; x < w; x++)
+					{
+						int l = image[x, y].R;
+						if (y > 0 && y < h - 1 && x > 0 && x < w - 1)
+						{
+							l = l * 5 - image[x, y - 1].R - image[x, y + 1].R - image[x - 1, y].R - image[x + 1, y].R;
+							if (l < 0) l = 0;
+							if (l > 255) l = 255;
+						}
+						sb.Append(chars[l * 14 / 255]);
+					}
+					sb.Append("<br/>");
+				}
+			}
+			sb.Append(html_tail);
+
+			outStream.Dispose();
+
+			return sb.ToString().Replace("...", ".. ");
+		}
+	}
+}
---
+++ ImageAsciify/Program.cs
@@ -0,0 +1,37 @@
+﻿using CommandLine;
+using System;
+using System.IO;
+
+namespace ImageAsciify
+{
+	class Program
+	{
+		public class Options
+		{
+			[Option('g', HelpText = "直方图均衡化", Required = false, Default = false)]
+			public bool HistogramEqualization { get; set; }
+
+			[Value(0, HelpText = "图像文件路径", Required = true)]
+			public string FilePath { get; set; }
+
+			[Option('l', Default = 512, HelpText = "最大像素", Required = false)]
+			public int MaxLength { get; set; }
+		}
+
+		static void Main(string[] args)
+		{
+			Parser.Default.ParseArguments<Options>(args)
+				.WithParsed(o =>
+				{
+					var html = Asciify.ImageToHTML(o.FilePath, o.HistogramEqualization, o.MaxLength);
+					string tempFile = Path.GetTempFileName() + ".html";
+					File.WriteAllText(tempFile, html);
+					System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo()
+					{
+						UseShellExecute = true,
+						FileName = tempFile
+					});
+				});
+		}
+	}
+}
